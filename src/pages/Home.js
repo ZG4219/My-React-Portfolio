a029d6c615ded0a3dc30c2f83a9e10139b2f51ab
Dionne Angeles <jr8dionne@yahoo.com>
2023-03-01 11:52:37 -0800

    created home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="container mx-auto mt-10">
+      <p className="text-4xl font-bold mb-4">Hi, I'm Dionne Angeles</p>
+      <p className="text-lg mb-8">
+        I'm a full-stack developer with experience in React, Node.js, and MongoDB.
+      </p>
+      <h2 className="text-2xl font-bold mb-4">Skills</h2>
+      <ul className="list-disc pl-8">
+        <li>React</li>
+        <li>Node.js</li>
+        <li>MongoDB</li>
+        <li>Tailwind CSS</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
