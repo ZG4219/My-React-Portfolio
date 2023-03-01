e44abea9a54d21404e4be267cb4c703cfc66a95c
Dionne Angeles <jr8dionne@yahoo.com>
2023-03-01 11:59:13 -0800

    added contact and deleted logo.svg

---
+++ src/pages/Contact.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div className="container mx-auto mt-10">
+      <h2 className="text-4xl font-bold mb-8">Contact Me</h2>
+      <p className="text-lg mb-4">Email: jr8dionne@gmail.com</p>
+      <a
+        href="https://github.com/ZG4219/My-React-Portfolio"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-gray-700 text-white px-4 py-2 rounded-md hover:bg-gray-800"
+      >
+        GitHub
+      </a>
+    </div>
+  );
+}
+
+export default Contact;
